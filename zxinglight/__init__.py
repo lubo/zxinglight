7f9be3b00af5de563d1a84be24ef1e253bc7a2bf
Nick <nick@kousu.ca>
2019-03-31 03:34:45 -0400

    Make cv2 a first-class citizen.
    cv2 is the fastest way to access a webcam (e.g. if you're trying to make a barcode scanner) in python,
    but the zxinglight API forces PIL, even though the QR library doesn't care.

--- zxinglight/__init__.py
+++ zxinglight/__init__.py
@@ -1,7 +1,15 @@
 # -*- coding: utf-8 -*-
 
 from enum import IntEnum, unique
-from PIL import Image
+try:
+    from PIL import Image
+except ImportError:
+    pass
+try:
+    import cv2
+    import numpy as np
+except ImportError:
+    pass
 
 from ._zxinglight import zxing_read_codes
 
@@ -87,16 +95,27 @@ def read_codes_full(image, barcode_type=BarcodeType.NONE,
         https://zxing.github.io/zxing/apidocs/com/google/zxing/Binarizer.html
     """
 
-    if not Image.isImageType(image):
-        raise ValueError('Provided image is not a PIL image')
+    if "Image" in globals() and Image.isImageType(image):
+        grayscale_image = image.convert('L')
+
+        raw_image = grayscale_image.tobytes()
+        width, height = grayscale_image.size
+    elif "cv2" in globals() and isinstance(image, np.ndarray):
+        if image.dtype != np.uint8:
+            raise TypeError("Provided cv2 image is not in integer bitdepth.")
+        if len(image.shape) == 2:
+            greyscale_image = image
+        elif len(image.shape) == 3 and image.shape[-1] == 3:
+            greyscale_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        else:
+            raise TypeError("Provided cv2 image is not in BGR nor greyscale formats.")
+        raw_image = greyscale_image.tobytes()
+        height, width = image.shape[:2]
+    else:
+        raise TypeError('Provided image is not a PIL nor cv2 image')
 
     if not isinstance(barcode_type, BarcodeType):
         raise ValueError('barcode_type is not an enum member of BarcodeType')
-
-    grayscale_image = image.convert('L')
-
-    raw_image = grayscale_image.tobytes()
-    width, height = grayscale_image.size
 
     return zxing_read_codes(raw_image, width, height, barcode_type, try_harder, hybrid, multi)
 
